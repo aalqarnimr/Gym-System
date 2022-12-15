86d265396093b29352eb9c80b9438a1f0c60b8c6
Abdulrahmanaak <Abdulrahman1aak@gmail.com>
2022-12-15 14:27:53 +0300

    Design the home page

---
+++ src/main/java/com/example/demo3/homePageController.java
@@ -0,0 +1,22 @@
+package com.example.demo3;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class homePageController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToPlanList(ActionEvent event){
+
+    }
+    public void switchToHomePage(ActionEvent event){
+
+    }
+    public void switchToAddPlan(ActionEvent event){
+
+    }
+}
