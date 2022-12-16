01861fad672e42529a8e7c6914fa8f6671255063
iSullya <czxcz.11@gmail.com>
2022-12-16 22:31:51 +0300

    trainer page linked to mainpage

--- src/main/java/com/example/demo3/traineeAccount.java
+++ src/main/java/com/example/demo3/traineeAccount.java
@@ -2,6 +2,8 @@ package com.example.demo3;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,6 +16,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class traineeAccount {
     private Stage stage;
@@ -78,6 +81,14 @@ public class traineeAccount {
         catch(IllegalArgumentException e){
             System.out.println("no image selected");
         }
+    }
+
+    public void toMainPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 
