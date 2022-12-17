d7ba119e2552d4846b62087e081f2ab57522ca59
aalqarnimr <51996693+aalqarnimr@users.noreply.github.com>
2022-12-17 19:37:53 +0300

    Revert "Update module-info.java"
    This reverts commit 31601ebb477437e42d73f9b3de205b68ee82e450.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.demo3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
-    requires javafx.media;
 
 
     opens com.example.demo3 to javafx.fxml;
