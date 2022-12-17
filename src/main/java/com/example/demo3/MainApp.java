1d92cc2509924ba620d13ebe83e6ac171a0370ab
aalqarnimr <51996693+aalqarnimr@users.noreply.github.com>
2022-12-17 19:37:29 +0300

    Revert "test changes"
    This reverts commit b168fcf5ef4db7eedcd82e03812634bb18e2df30.

--- src/main/java/com/example/demo3/MainApp.java
+++ src/main/java/com/example/demo3/MainApp.java
@@ -2,7 +2,6 @@ package com.example.demo3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -11,16 +10,8 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         APIComm.getInfo();
-
-        //This code for testing.
-        Parent root = FXMLLoader.load(getClass().getResource("workoutPage.fxml"));
-        Scene scene = new Scene(root);
-
-        //Uncomment these lines when the testing process ends.
-        //FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("wlcPage.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 360, 600);
-        //---
-
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("wlcPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 360, 600);
         stage.setTitle("GymApp");
         stage.setScene(scene);
         stage.show();
